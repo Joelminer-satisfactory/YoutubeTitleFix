fab452f476fff6c27fd521c021d4b14524b9ad69
Joelminer <joelvanvuuren@gmail.com>
2024-09-24 06:46:55 +0200

    complete rewrite for better ux
    - changed to tabs.onUpdated for more reliable detection.
    - added h1 to display the title on video pages to circumvent an annoying bug.

--- background.js
+++ background.js
@@ -1,31 +1,30 @@
-try{
-    let isPaused = false;
-    browser.webNavigation.onHistoryStateUpdated.addListener( () => {
-        console.log("Paused: ", isPaused)
-        if (!isPaused){
-            isPaused = true;
-            console.log("updated")
-            browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
-                const tabId = tabs[0].id;
-                console.log(tabId, "sending termination message")
-                browser.tabs.sendMessage(tabId, { message: "terminate" });
-            });
-            browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
-                const tabId = tabs[0].id;
-                console.log(/youtube\.com/.test(tabs[0].url))
-                if (/youtube\.com/.test(tabs[0].url)){
-                    browser.tabs.update(tabId, {url: tabs[0].url})
-                }
-                browser.tabs.executeScript(tabId, { file: "content.js" });
-        })}
-        else{
-            console.info("script paused, not triggering")
-        };
-    })
-    browser.webNavigation.onCompleted.addListener(() => {
-        isPaused = false;
-    })
+browser.tabs.onUpdated.addListener(updateHandler)
+let isPaused = false
+async function updateHandler(tabId, changeInfo, changedTab){
+    if (isPaused){
+        return
+    }
+    if (changeInfo.url !== undefined && changeInfo.status !== "loading"){
+        isPaused = true
+        console.log("TabId: ", tabId, "Change info: ", changeInfo, "New status: ", changedTab.status)
+
+        browser.tabs.query({ active: true, currentWindow: true }).then(async tabs => {
+            const tabId = tabs[0].id;
+            console.log("Tab ID:", tabId, "sending termination message")
+            browser.tabs.sendMessage(tabId, { message: "terminate" });
+            if (/.\.youtube\.com\/$/.test(changeInfo.url) || /.\.youtube\.com$/.test(changeInfo.url)){
+                await sleep(500)
+                console.log("reloading tab")
+                browser.tabs.update(tabId, {url: tabs[0].url})
+            }
+            await sleep(500)
+            console.info("inserting script")
+            browser.tabs.executeScript(tabId, { file: "content.js" });
+        });
+        await sleep(500)
+        isPaused = false
+    }
 }
-catch (error){
-    console.log(error.message)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
--- content.js
+++ content.js
@@ -11,7 +11,6 @@ const targetNode = document.querySelector("body");
 const config = { attributes: false, childList: true, subtree: true };
 
 const modify_titles = async (mutationlist, observer) => {
-    console.info("modifying titles")
     let videoTitles = [];
     let selectors = [
         "yt-formatted-string#video-title", 
@@ -23,7 +22,42 @@ const modify_titles = async (mutationlist, observer) => {
         let elements = document.querySelectorAll(selector);
         elements.forEach((el) => {videoTitles.push(el)})
     });
+    let element = document.querySelector("div#title.style-scope.ytd-watch-metadata")
+    element.style.display = 'none'
     observer.disconnect();
+    //get and display the title on a video page
+    let url = window.location.href;
+    let match = url.match(/(?<=v=)[^&#]+/);
+    let videoId;
+    if (match){
+        videoId = match[0];
+    }
+    if(/youtube\.com\/watch\?v=/.test(url)){
+        fetch(`https://www.youtube.com/oembed?url=https%3A//youtube.com/watch%3Fv%3D${videoId}&format=json`)
+            .then(response => response.json())
+            .then(data => {
+                let title = data.title
+                let newTitle = title.toLowerCase();
+                var rg = /(^\s*\w{1}|\.\s*\w{1})/gi;
+                newTitle = newTitle.replace(rg, function(toReplace) {
+                    return toReplace.toUpperCase();
+                });
+                if (!document.querySelector("div#titleDiv")){
+                    let element = document.querySelector("div#above-the-fold.style-scope.ytd-watch-metadata")
+                    let newDiv = document.createElement("div")
+                    newDiv.id = "titleDiv"
+                    let h1Element = document.createElement("h1")
+                    h1Element.textContent = newTitle
+                    h1Element.id = "titleH1"
+                    newDiv.appendChild(h1Element)
+                    element.insertBefore(newDiv, element.firstChild)
+                }
+                let element = document.querySelector("h1#titleH1")
+                if (element.textContent != newTitle){
+                    element.textContent = newTitle
+                }
+            })
+    }
     // Modify the text content of each element
     videoTitles.forEach((element, index) => {
         if (element.hasAttribute("aria-label")){
@@ -56,6 +90,7 @@ let future_observer;
 const observer = new MutationObserver(modify_titles, future_observer);
 future_observer = observer;
 observer.observe(targetNode, config)
+console.log("script inserted, started observing for changes")
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
