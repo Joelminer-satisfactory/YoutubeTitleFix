4446c646a1f83faef768d422c52160116b5f0afd
Joelminer <89350141+Joelminer-satisfactory@users.noreply.github.com>
2024-09-28 17:17:52 +0200

    Merge Dev (#2)
    * complete rewrite for better ux
    
    - changed to tabs.onUpdated for more reliable detection.
    - added h1 to display the title on video pages to circumvent an annoying bug.
    
    * v3.1.0
    
    changed request handling to check for multiple updates in quick succession, and to only handle the last one in such a case.
    
    * Update YoutubeTitleFix.xpi
    
    * v3.1.1
    
    changed to only reload the tab if on the homepage and the destination url matches the source url
    
    * v3.1.1
    
    -update xpi package
    -improved speed, removed unnecessary lines
    
    * add check to avoid unneeded reloading
    
    * Update YoutubeTitleFix.xpi

--- background.js
+++ background.js
@@ -1,31 +1,60 @@
-try{
-    let isPaused = false;
-    browser.webNavigation.onHistoryStateUpdated.addListener( () => {
-        console.log("Paused: ", isPaused)
-        if (!isPaused){
-            isPaused = true;
-            console.log("updated")
-            browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
-                const tabId = tabs[0].id;
-                console.log(tabId, "sending termination message")
-                browser.tabs.sendMessage(tabId, { message: "terminate" });
-            });
-            browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
-                const tabId = tabs[0].id;
-                console.log(/youtube\.com/.test(tabs[0].url))
-                if (/youtube\.com/.test(tabs[0].url)){
+browser.tabs.onUpdated.addListener(updateHandler)
+let isPaused = false
+let isAwaitingRequest = false;
+let recievedResponse = false;
+async function updateHandler(tabId, changeInfo, changedTab){
+    if (isPaused){
+        return
+    }
+    if (changeInfo.url !== undefined && changeInfo.status !== "loading"){
+        isPaused = true
+        console.log("TabId: ", tabId, "Change info: ", changeInfo, "New status: ", changedTab.status)
+        let urlBeforeNav;
+        browser.tabs.query({ active: true, currentWindow: true }).then(async tabs => {
+            
+            console.log("Tab ID:", tabId, "sending termination message")
+            browser.tabs.sendMessage(tabId, { message: "terminate" })
+            browser.runtime.onMessage.addListener((request, sender, sendResponse) =>{
+                recievedResponse = true
+                urlBeforeNav = request.message
+            })
+            
+            // 
+             //if actively awaiting request, stop awaiting and continue
+            if(isAwaitingRequest){
+                isAwaitingRequest = false
+            }
+            /*if not awaiting request, start the wait by changing the variable to true, and waiting 1000ms (1s), 
+            if still awaiting after that, no longer wait and proceed. If no longer waiting, quit, because another request has come in shortly after*/
+            else if(!isAwaitingRequest){
+                isAwaitingRequest = true
+                await sleep(500)
+                if (isAwaitingRequest){
+                    isAwaitingRequest = false
+                }
+                else{
+                    return
+                }
+            }
+            await sleep(100)
+            console.log("Current: ", changeInfo.url, "Previous: ", urlBeforeNav)
+            if (/.\.youtube\.com\/$/.test(changeInfo.url)/* && changeInfo.url === urlBeforeNav*/ || /.\.youtube\.com$/.test(changeInfo.url)/* && changeInfo.url === urlBeforeNav*/){
+                if(changeInfo.url.indexOf("t=") === -1){
+                    console.log("reloading tab")
                     browser.tabs.update(tabId, {url: tabs[0].url})
                 }
-                browser.tabs.executeScript(tabId, { file: "content.js" });
-        })}
-        else{
-            console.info("script paused, not triggering")
-        };
-    })
-    browser.webNavigation.onCompleted.addListener(() => {
-        isPaused = false;
-    })
+                
+            }
+            
+            console.info("inserting script")
+            browser.tabs.executeScript(tabId, { file: "content.js" });
+            // await sleep(100)
+            // browser.tabs.sendMessage(tabId, {message: "fetchUrl"})
+        });
+        
+        isPaused = false
+    }
 }
-catch (error){
-    console.log(error.message)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
--- content.js
+++ content.js
@@ -1,9 +1,15 @@
-browser.runtime.onMessage.addListener((request) => {
+let currentWindowUrl;
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received message:", request);
     if (request.message === "terminate"){
         console.log("Content script terminating");
-        
-        throw new Error("Termination requested");
+        browser.runtime.sendMessage({message: currentWindowUrl})
+        console.log("sent message: ", currentWindowUrl)
+        throw new error("terminating")
+    }
+    else if(request.message === "fetchUrl"){
+        currentWindowUrl = window.location.href
+        console.log("Recieved, setting url to: ", currentWindowUrl)
     }
 });
 
@@ -11,7 +17,6 @@ const targetNode = document.querySelector("body");
 const config = { attributes: false, childList: true, subtree: true };
 
 const modify_titles = async (mutationlist, observer) => {
-    console.info("modifying titles")
     let videoTitles = [];
     let selectors = [
         "yt-formatted-string#video-title", 
@@ -23,7 +28,47 @@ const modify_titles = async (mutationlist, observer) => {
         let elements = document.querySelectorAll(selector);
         elements.forEach((el) => {videoTitles.push(el)})
     });
+    let element = document.querySelector("div#title.style-scope.ytd-watch-metadata")
+    try{
+        element.style.display = 'none'
+    }
+    catch (error){
+        console.log(error.message)
+    }
     observer.disconnect();
+    //get and display the title on a video page
+    let url = window.location.href;
+    let match = url.match(/(?<=v=)[^&#]+/);
+    let videoId;
+    if (match){
+        videoId = match[0];
+    }
+    if(/youtube\.com\/watch\?v=/.test(url)){
+        fetch(`https://www.youtube.com/oembed?url=https%3A//youtube.com/watch%3Fv%3D${videoId}&format=json`)
+            .then(response => response.json())
+            .then(data => {
+                let title = data.title
+                let newTitle = title.toLowerCase();
+                var rg = /(^\s*\w{1}|\.\s*\w{1})/gi;
+                newTitle = newTitle.replace(rg, function(toReplace) {
+                    return toReplace.toUpperCase();
+                });
+                if (!document.querySelector("div#titleDiv")){
+                    let element = document.querySelector("div#above-the-fold.style-scope.ytd-watch-metadata")
+                    let newDiv = document.createElement("div")
+                    newDiv.id = "titleDiv"
+                    let h1Element = document.createElement("h1")
+                    h1Element.textContent = newTitle
+                    h1Element.id = "titleH1"
+                    newDiv.appendChild(h1Element)
+                    element.insertBefore(newDiv, element.firstChild)
+                }
+                let element = document.querySelector("h1#titleH1")
+                if (element.textContent != newTitle){
+                    element.textContent = newTitle
+                }
+            })
+    }
     // Modify the text content of each element
     videoTitles.forEach((element, index) => {
         if (element.hasAttribute("aria-label")){
@@ -56,6 +101,9 @@ let future_observer;
 const observer = new MutationObserver(modify_titles, future_observer);
 future_observer = observer;
 observer.observe(targetNode, config)
+console.log("script inserted, started observing for changes")
+currentWindowUrl = window.location.href
+console.log("current window url: ", window.location.href)
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
